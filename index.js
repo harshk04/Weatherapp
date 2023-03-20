65f3ac16c5d359923a3542e553edf52c05aa6249
harshk04 <kumawatharsh2004@gmail.com>
2023-03-21 00:52:39 +0530

    c

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' +city, opt
     document.getElementById("humidity").innerHTML=response.humidity
     document.getElementById("temp").innerHTML=response.temp
     document.getElementById("wind").innerHTML=response.wind_speed
+    
   })
 
   
@@ -32,33 +33,33 @@ fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' +city, opt
     
     e.preventDefault()
 getWeather(city.value)
-
+document.getElementById("cityName").innerHTML=city.value
   })
 
   getWeather("City ")
-  let city=document.querySelector('input');
 
-  const ai = {
-    method: 'POST',
-    headers: {
-      accept: 'application/json',
-      'content-type': 'application/json',
-      'X-API-KEY': '663d1402-30f0-4a43-90c2-877f2b61dc55'
-    },
-    body: JSON.stringify({
-      enable_google_results: 'true',
-      enable_memory: false,
-      input_text: `About the weather in ${city.value} in about 20 words`
-    })
-  };
+
+  // const ai = {
+  //   method: 'POST',
+  //   headers: {
+  //     accept: 'application/json',
+  //     'content-type': 'application/json',
+  //     'X-API-KEY': '663d1402-30f0-4a43-90c2-877f2b61dc55'
+  //   },
+  //   body: JSON.stringify({
+  //     enable_google_results: 'true',
+  //     enable_memory: false,
+  //     input_text: `About the weather in ${city.value} in about 20 words`
+  //   })
+  // };
   
-  fetch('https://api.writesonic.com/v2/business/content/chatsonic?engine=premium', ai)
-    .then(response => response.json())
-    .then(response => {
-        console.log(response)
-        document.getElementById("text").innerHTML=response.message
+  // fetch('https://api.writesonic.com/v2/business/content/chatsonic?engine=premium', ai)
+  //   .then(response => response.json())
+  //   .then(response => {
+  //       console.log(response)
+  //       document.getElementById("text").innerHTML=response.message
  
-    })
-    .catch(err => console.error(err));
+  //   })
+  //   .catch(err => console.error(err));
 
   
