3e6d2e7d6ee47859a6660ee73db35972af769f36
HARSH KUMAWAT <115946158+harshk04@users.noreply.github.com>
2023-03-21 00:03:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,5 +38,27 @@ getWeather(city.value)
   getWeather("City :")
   let city=document.querySelector('input');
 
+  const ai = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      'X-API-KEY': '663d1402-30f0-4a43-90c2-877f2b61dc55'
+    },
+    body: JSON.stringify({
+      enable_google_results: 'true',
+      enable_memory: false,
+      input_text: `About the weather in ${city.value} in about 20 words`
+    })
+  };
   
+  fetch('https://api.writesonic.com/v2/business/content/chatsonic?engine=premium', ai)
+    .then(response => response.json())
+    .then(response => {
+        console.log(response)
+        document.getElementById("text").innerHTML=response.message
+ 
+    })
+    .catch(err => console.error(err));
+
   
